c63988c69b774be3718409c3ccc0d2b38adcd204
Andrew Husiev <andrew.husiev@gmail.com>
2025-05-21 12:51:57 -0700

    Add room api fixes for endpoints

--- src/controllers/rooms/rooms-create.js
+++ src/controllers/rooms/rooms-create.js
@@ -2,7 +2,7 @@ import model from "../../models/index.js";
 import asyncHandler from "../../utils/async-handler.js";
 
 const roomsCreate = asyncHandler(async (req, res) => {
-  const { name, isGroup, members, createdBy } = req.data;
+  const { name, isGroup, members, createdBy } = req.body;
   const room = model.ROOM.create({
     name,
     isGroup,
--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -3,12 +3,12 @@ import controller from "../controllers/index.js";
 
 const router = Router();
 
-router.post("/rooms", controller.rooms.create);
-router.get("/rooms/:id", controller.rooms.getById);
+router.post("/", controller.rooms.create);
+router.get("/:id", controller.rooms.getById);
 router.get("/users/:userId/rooms", controller.rooms.getAllByUserId);
-router.put("/rooms/:id", controller.rooms.update);
-router.delete("/rooms/:id", controller.rooms.delete);
-router.post("/rooms/:id/members", controller.rooms.addMember);
-router.delete("/rooms/:id/members/:uid", controller.rooms.delete);
+router.put("/:id", controller.rooms.update);
+router.delete("/:id", controller.rooms.delete);
+router.post("/:id/members", controller.rooms.addMember);
+router.delete("/:id/members/:uid", controller.rooms.delete);
 
 export default router;
