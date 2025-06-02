f59beece9cd604de049801fa9185a6e65d88d941
Andrew Husiev <andrew.husiev@gmail.com>
2025-06-02 14:22:39 -0700

    Add minor fixes

--- src/controllers/rooms/rooms-create.js
+++ src/controllers/rooms/rooms-create.js
@@ -5,23 +5,53 @@ import { createError } from '../../utils/error-helpers.js';
 const roomsCreate = asyncHandler(async (req, res) => {
   const { name, isGroup, members, createdBy } = req.body;
 
-  if (!name) throw createError('Field missed: name');
-  if (!members) throw createError('Field missed: members');
-  if (!createdBy) throw createError('Field missed: createdBy');
+  if (!name) throw createError('Missing required field: name');
+
+  if (!Array.isArray(members) || members.length === 0) {
+    throw createError('Missing or invalid members array');
+  }
+
+  if (!createdBy) throw createError('Missing required field: createdBy');
 
   const user = await model.USER.findById(createdBy);
   if (!user) throw createError('Invalid user ID');
 
-  const uniqueMembers = new Set([...members, createdBy]);
-  const validUsers = await model.USER.find({ _id: { $in: [...uniqueMembers] } });
+  // Ensure unique members list, including creator
+  const uniqueMembers = Array.from(new Set([...members, createdBy]));
 
-  if (validUsers.length !== uniqueMembers.size) {
+  const validUsers = await model.USER.find({ _id: { $in: uniqueMembers } });
+  if (validUsers.length !== uniqueMembers.length) {
     throw createError('One or more user IDs are invalid');
   }
 
-  const newRoom = await model.ROOM.create({ name, isGroup, members, createdBy });
+  let room;
 
-  const populatedRoom = await model.ROOM.findById(newRoom._id)
+  room = await model.ROOM.findOne({
+    isGroup,
+    members: { $all: uniqueMembers, $size: uniqueMembers.length },
+  });
+
+  if (room) {
+    await model.ROOM.updateOne(
+      {
+        _id: room._id,
+      },
+      {
+        $pull: {
+          deletedFor: createdBy,
+        },
+      }
+    );
+  } else {
+    room = await model.ROOM.create({
+      name,
+      isGroup,
+      members: uniqueMembers,
+      createdBy,
+    });
+  }
+
+  const populatedRoom = await model.ROOM.findById(room._id)
     .populate('members', 'username email')
     .populate('createdBy', 'username');
 
